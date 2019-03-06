fba9a3d54b5e04412bd0a380447607d6be4a7fc5
b_lagouge <benedicte.lagouge@lucielabs.com>
2019-03-06 10:34:50 +0100

    s

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,10 @@
 var express = require('express');
 module.exports = function(app) {
 var apiRoutes =  express.Router();
+
+app.get('/',function(req,res){
+    res.send('BLOUBLOUBLOU');
+  });
 app.get('/',function(req,res){
     res.send('received data');
     console.log(req.body);
